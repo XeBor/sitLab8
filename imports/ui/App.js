6a58c1bdf23f3e9c5cde6093e70b54d3d6e2a87b
XeBor <55808155+XeBor@users.noreply.github.com>
2020-04-24 17:16:34 +0300

    filtering data with publish and subscribe

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -42,9 +42,18 @@ Meteor.call('tasks.insert', text);
     if (this.state.hideCompleted) {
       filteredTasks = filteredTasks.filter(task => !task.checked);
     }
-    return filteredTasks.map((task) => (
-      <Task key={task._id} task={task} />
-    ));
+    return filteredTasks.map((task) => {
+        const currentUserId = this.props.currentUser && this.props.currentUser._id;
+        const showPrivateButton = task.owner === currentUserId;
+
+        return (
+          <Task
+            key={task._id}
+            task={task}
+            showPrivateButton={showPrivateButton}
+          />
+        );
+      });
   }
   render() {
     return (
@@ -81,6 +90,7 @@ Meteor.call('tasks.insert', text);
 }
 
 export default withTracker(() => {
+   Meteor.subscribe('tasks');
 	return {
 	  tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
     incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
